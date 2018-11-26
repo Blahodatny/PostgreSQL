f6a71a45d320604c40ee89c9c12b6c80508679fd
Dima <dbrlmola82@gmail.com>
2018-11-26 22:00:50 +0200

    Push-mush

--- lab/src/Main.java
+++ lab/src/Main.java
@@ -1,6 +1,19 @@
-public class Main {
+import java.sql.Connection;
+import java.sql.DriverManager;
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+public class Main {
+    public static void main(String args[]) {
+        Connection c = null;
+        try {
+            Class.forName("org.postgresql.Driver");
+            c = DriverManager
+                    .getConnection("jdbc:postgresql://localhost:5432/testdb",
+                            "postgres", "123");
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.err.println(e.getClass().getName()+": "+e.getMessage());
+            System.exit(0);
+        }
+        System.out.println("Opened database successfully");
     }
 }
