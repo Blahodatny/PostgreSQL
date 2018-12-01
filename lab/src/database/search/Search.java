616d060aee2627fbbbad80d9227b022b5663bba6
Dima <dbrlmola82@gmail.com>
2018-12-01 17:16:45 +0200

    Added class Search

---
+++ lab/src/database/search/Search.java
@@ -0,0 +1,11 @@
+package database.search;
+
+import database.RetailService;
+
+import java.sql.SQLException;
+
+public class Search extends RetailService {
+    // CUSTOMERS.Street && CUSTOMERS.City + ORDERS.ToStreet && ORDERS.ToCity
+    public void search(String string) {
+    }
+}
