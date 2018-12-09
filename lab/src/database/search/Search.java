c606aad138512d9beebd5ed4c220b795e53be630
Dimon <dbrlmola82@gmail.com>
2018-12-09 21:41:21 +0200

    task2 begin

--- lab/src/database/search/Search.java
+++ lab/src/database/search/Search.java
@@ -2,8 +2,6 @@ package database.search;
 
 import database.RetailService;
 
-import java.sql.SQLException;
-
 public class Search extends RetailService {
     // CUSTOMERS.Street && CUSTOMERS.City + ORDERS.ToStreet && ORDERS.ToCity
     public void search(String string) {
